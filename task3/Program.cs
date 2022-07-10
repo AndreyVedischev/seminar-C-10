34393b28b405db1bfad73984d64613c5599f8992
Andrey Vedischev <vedischev76@mail.ru>
2022-07-10 18:55:14 +0500

    add task 3

---
+++ task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int[] data = { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
+int[] info = { 2, 3, 3, 1 };
+
+if (GetSum(info) != data.Length)
+{
+    WriteLine("Неверная длинна данных.");
+    return;
+}
+WriteLine(String.Join(" ", RezultNum(data, info)));
+
+
+
+int[] RezultNum(int[] datArr, int[] infArr)
+{
+    int[] result = new int[infArr.Length];
+    string dat = String.Join("", datArr);
+    for (int i = 0; i < result.Length; i++)
+    {
+        string subData = dat.Substring(0, infArr[i]);
+        result[i] = Convert.ToInt32(subData, 2);
+        dat = dat.Remove(0, infArr[i]);
+    }
+    return result;
+}
+
+int GetSum(int[] array)
+{
+    int sum = 0;
+    foreach (int item in array)
+    {
+        sum += item;
+    }
+    return sum;
+}
