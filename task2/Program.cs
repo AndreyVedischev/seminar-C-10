9d3e10c10fa6970159569cde64bca503f99a0acd
Andrey Vedischev <vedischev76@mail.ru>
2022-07-10 16:35:15 +0500

    add task 2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите длинну слова: ");
+int n = int.Parse(ReadLine());
+string alfavite= "аисе";
+
+MnogoSlov(alfavite, n,"");
+
+void MnogoSlov(string alf,int dlinna, string fix)
+{
+    if(dlinna ==0)
+      WriteLine(fix);    
+    else    
+        foreach(char c in alf) MnogoSlov(alf, dlinna-1, fix+c);   
+
+}
+    
