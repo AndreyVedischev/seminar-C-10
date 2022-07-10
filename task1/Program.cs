19ae8e1a311cf1c822a02ff9470ccba31e09aacd
Andrey Vedischev <vedischev76@mail.ru>
2022-07-10 15:41:44 +0500

    add task 1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите первое число ряда: ");
+int n = int.Parse(ReadLine());
+WriteLine("Введите второе число ряда: ");
+int k = int.Parse(ReadLine());
+
+WriteLine("Введите длинну ряда: ");
+int stop = int.Parse(ReadLine());
+PosledSum(n,k,stop);
+
+void PosledSum(int number1, int number2, int i)
+{
+    if(i == 0)
+    {
+        return;
+    }
+    Write($"{number1}, ");
+    PosledSum(number2,number1+number2,i-1);    
+}
