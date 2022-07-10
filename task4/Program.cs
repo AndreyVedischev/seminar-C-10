071a83c87ddf5a63860d287077e3099752e451ac
Andrey Vedischev <vedischev76@mail.ru>
2022-07-10 18:55:25 +0500

    add task 4

---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine(String.Join(", ", RezultNum(new int[] { 0, 1, 1, 1, 1, 0, 0, 0, 1 }, new int[] { 2, 3, 3, 1 })));
+
+double[] RezultNum(int[] dat, int[] inf)
+{
+    double[] result = new double[inf.Length];
+    int count = 0;
+    for (int i = 0; i < inf.Length; i++)
+    {
+        for (int j = 0; j < inf[i]; j++)
+        {
+            result[i] += dat[count] * Math.Pow(2, inf[i]-j - 1);
+            count++;
+        }
+    }
+    return result;
+}
